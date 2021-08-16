9ac2657f2fbe436107021449fe912e74652e1705
ShrutiSemwal <semwalshruti450@gmail.com>
2021-08-16 19:37:41 +0530

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,46 @@
+function Footer() {
+    return (
+        <div className="grid grid-cols-1 md:grid-cols-4 space-y-10 
+        px-32 py-14 bg-gray-100 text-gray-600">
+
+            <div className="space-y-4 text-xs text-gray-800">
+           <h5 className="font-bold">ABOUT</h5>
+           <p>How Airbnb works</p>
+           <p>Newsroom</p>
+           <p>Investers</p>
+           <p>Airbnb Plus</p>
+           <p>Airbnb Luxe</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+           <h5 className="font-bold">COMMUNITY</h5>
+           <p>Accessibility</p>
+           <p>Not a real site</p>
+           <p>It's a pretty awesome clone</p>
+           <p>Referrals accepted</p>
+           <p>Shruti Semwal</p>
+            </div>
+
+            <div className="space-y-4 text-xs text-gray-800">
+           <h5 className="font-bold">HOST</h5>
+           <p>Shruti Semwal</p>
+           <p>Presents</p>
+           <p>Projects on</p>
+           <p>Clone Web-app</p>
+           <p>Airbnb Next.js App</p>
+            </div>
+            
+            <div className="space-y-4 text-xs text-gray-800">
+           <h5 className="font-bold">SUPPORT</h5>
+           <p>Help Centre</p>
+           <p>Trust & Safety</p>
+           <p>Say Hi everyone</p>
+           <p>If any query</p>
+           <p>contact me</p>
+            </div>
+           
+        </div>
+    )
+}
+
+export default Footer
