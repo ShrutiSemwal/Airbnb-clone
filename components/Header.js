a652aea2a0b8c646f19c45ef31b8de743ae86a9e
ShrutiSemwal <semwalshruti450@gmail.com>
2021-08-19 09:51:37 +0530

    Designed InfoCard

--- components/Header.js
+++ components/Header.js
@@ -8,13 +8,15 @@ import {GlobeAltIcon,
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRangePicker } from 'react-date-range';
+import { useRouter } from 'next/dist/client/router';
 
 
-function Header() {
+function Header({placeholder}) {
     const [searchInput, setSearchInput] = useState('');
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setEndDate]= useState(new Date());
     const [noOfGuests, setNoOfGuests] = useState(1);
+    const router = useRouter();
 
     const handleSelect = (ranges) => {
             setStartDate(ranges.selection.startDate);
@@ -23,6 +25,18 @@ function Header() {
 
     const resetInput= () => {
             setSearchInput("");
+    };
+
+    const search = () =>{
+        router.push({
+            pathname: '/search',
+            query: {
+                location: searchInput,
+                startDate: startDate.toISOString(),
+                endDate: endDate.toISOString(),
+                noOfGuests,
+            },
+        });
     };
 
     const selectionRange = {
@@ -38,7 +52,7 @@ function Header() {
          bg-white shadow-md py-5 px-5 md:px-10">
 
             {/* left*/}
-            <div className="relative flex items-center h-10
+            <div onClick={() => router.push('/')} className="relative flex items-center h-10
             cursor-pointer my-auto">
              <Image
                src="https://links.papareact.com/qd3"
@@ -58,7 +72,7 @@ function Header() {
                  outline-none text-sm text-gray-600
                  placeholder-gray-400" 
                 type="text" 
-                placeholder="Start your Search"/>
+                placeholder={placeholder || "Start your Search"}/>
                 <SearchIcon className=" hidden md:inline-flex h-8 bg-red-400 text-white rounded-full
                 p-2 cursor-pointer md:mx-2" />
             </div>
@@ -109,7 +123,7 @@ function Header() {
                        text-gray-500">
                         Cancel
                        </button>
-                       <button className="flex-grow
+                       <button onClick={search} className="flex-grow
                        text-red-400">
                         Search
                        </button>
--- pages/search.js
+++ pages/search.js
@@ -1,21 +1,55 @@
-import Footer from "../components/Footer"
-import Header from "../components/Header"
+import { useRouter } from "next/dist/client/router";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import {format} from 'date-fns';
+import InfoCard from "../components/InfoCard";
 
 
-function Search() {
+function Search({searchResults}) {
+    const router = useRouter();
+
+    console.log(searchResults);
+    const {location, startDate, endDate, noOfGuests} = router.query;
+
+    //ES6 destructuring
+    const formattedStartDate = format(new Date(startDate), "dd MMMM yy");
+    const formattedEndDate = format(new Date(endDate), "dd MMMM yy");
+    const range = `${formattedStartDate} - ${formattedEndDate}`;
     return (
-        <div className="h-screen">
-            <Header/>
+        <div className="">
+            <Header placeholder={`${location} | ${range} | ${noOfGuests} guests`} />
              <main className="flex ">
-                 <section>
-                     <p className="text-xs">300+ Stays for 5 number of guests</p>
+                 <section className="flex-grow pt-14 px-6">
+                     <p className="text-xs">300+ Stays -{range} for {noOfGuests} guests</p>
                      <h1 className="text-3xl font-semibold mt-2 mb-6">
-                         Stays in Mars</h1>
+                         Stays in {location} </h1>
 
-                    <div className="h">
-                        <p className="px-4 py-2 border rounded-full cursor-pointer hover:shadow-lg active:scale-95
-                        active:bg-gray-100 transition transform duration-100 ease-out">Cancellation Flexibility</p>
+                    <div className="hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap ">
+                        <p className="button">
+                            Cancellation Flexibility</p>
+                        <p className="button">
+                            Type of Place</p>
+                        <p className="button">
+                            Price</p>
+                        <p className="button">
+                            Rooms and Beds</p>
+                        <p className="button">
+                            More filters</p> {/* reusable utility classes*/}
+
                     </div>
+                   {searchResults.map(({img, location, title, description, star, price, total}) =>(
+                        <InfoCard
+                        key={img}
+                        img={img}
+                        location={location}
+                        title={title}
+                        description={description}
+                        star={star}
+                        price={price}
+                        total={total}
+
+                         />
+                   ))}
                  </section>
 
              </main>
@@ -25,3 +59,14 @@ function Search() {
 }
 
 export default Search
+
+export async function getServerSideProps() {
+    const searchResults = await fetch("https://links.papareact.com/isz").
+    then(res => res.json()); 
+
+    return {
+        props: {
+            searchResults,
+        },
+    };
+}
